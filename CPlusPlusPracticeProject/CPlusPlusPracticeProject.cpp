d0384c8b4b9407a86b713fb2dca62627f248b9d3
cameron boyle <cameronboyle7894@gmail.com>
2022-08-30 16:44:17 +0100

    added poem

--- CPlusPlusPracticeProject/CPlusPlusPracticeProject.cpp
+++ CPlusPlusPracticeProject/CPlusPlusPracticeProject.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Roses are red\n" << "Violets are blue\n" << "Python is slow\n" << "and so are you!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
